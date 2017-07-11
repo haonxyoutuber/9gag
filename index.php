505491dfef8e0651f1ecd392ba16ddb8890d3cb2
haonx <haonx.youtuber@outlook.com>
2017-07-11 23:54:12 +0700

    add Routing to App

--- index.php
+++ index.php
@@ -1,10 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-webstart
-</body>
-</html>
+<?php
+function route($name = 'default', $callback)
+{
+    if (isset($_GET['route'])) {
+        $currentRouteName = $_GET['route'];
+    }else{
+        $currentRouteName = 'default';
+    }
+    if ($name === $currentRouteName) {
+        $callback();
+        echo '<!--Route Name: '.$currentRouteName.'-->';
+    }
+}
+
+$indexFunctionName = function () {
+    echo '
+       <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <title>Title</title>
+            </head>
+            <body>
+                 webstart 2
+            </body>
+       </html>
+        ';
+};
+route('users.index', function(){
+    echo 'Route for users.index';
+});
+route('users.show', function(){
+    echo 'Route for users.show';
+});
+route('default', $indexFunctionName);
+?>
